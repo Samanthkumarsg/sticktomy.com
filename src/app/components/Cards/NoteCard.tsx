db672ff0f96fb9dd8af4080d6baa606495efd450
samanth kumar <samanthkumarsg@gmail.com>
2025-02-03 01:59:03 +0100

    Welcome to Stick To My . com

--- src/app/components/Cards/NoteCard.tsx
+++ src/app/components/Cards/NoteCard.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 export const NoteCard = () => {
     return (
-        <div className="flex flex-col items-start jusitify-start hover:animate-pulse">
+        <div className="flex flex-col items-start jusitify-start ">
 
-            <div className=" rounded-md w-full flex flex-col items-start justify-start p-4  bg-indigo-50 shadow shadow-indigo-100 -rotate-1  hover:animate-pulse duration-75 cursor-pointer  ">
+            <div className=" rounded-md w-full flex flex-col items-start justify-start p-4  bg-indigo-50 shadow shadow-indigo-100 -rotate-1 hover:ring-2 hover:ring-indigo-500 hover:ring-offset-4 duration-75 cursor-pointer  ">
                 <h1 className=" text-indigo-800 font-semibold tracking-wide text-sm mb-2 ">Create Team and prototype test with clients</h1>
 
                 <div className="flex flex-row items-center justify-between mt-2" >
--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,14 +1,33 @@
+"use client";
 import { FiPlus, FiUser } from "react-icons/fi";
 import { NoteCard } from "../components/Cards/NoteCard";
-
+import { getHours, format } from "date-fns";
+import { useState, useEffect } from "react";
 
 export default function Hello() {
+
+    const [timeOfDay, setTimeOfDay] = useState<String | null>(null);
+    const [today, setToday] = useState<String | null>("");
+
+
+    useEffect(() => {
+        const hours = getHours(new Date());
+        if (hours < 12) setTimeOfDay("Morning");
+        else if (hours < 18) setTimeOfDay("Afternoon");
+        else setTimeOfDay("Evening");
+
+        const formattedDate = format(new Date(), "EEEE, dd MMMM");
+        setToday(formattedDate);
+
+    }, []);
+
+
     return (
-        <div className="max-w-screen-lg border-indigo-300 rounded-xl my-8  mx-auto bg-white p-6">
-            <div className="flex flex-row items-end justify-between  py-6 borde border-indigo-300 sticky top-0  z-10 ">
+        <div className="max-w-screen-lg border-indigo-300 rounded-xl my-6  mx-auto bg-white p-6">
+            <div className="flex flex-row items-end justify-between  py-6 borde border-indigo-300 sticky top-0  z-10  bg-white ">
                 <div className="flex flex-col items-start justify-start ">
-                    <h1 className="text-md font-semibold text-indigo-800 tracking-wide">Hello, Samanth!</h1>
-                    <h6 className="text-sm font-medium text-indigo-700">Friday , 31 January</h6>
+                    <h1 className="text-md font-semibold text-indigo-800 tracking-wide">Good {timeOfDay}, Samanth!</h1>
+                    <h6 className="text-sm font-medium text-indigo-700">{today}</h6>
                 </div>
                 <div className="flex flex-row items-center justify-center ">
                     <div className="">
@@ -27,43 +46,20 @@ export default function Hello() {
                 </div>
 
             </div>
+
+            <h1 className="text-xl font-semibold text-indigo-800 my-4">Today</h1>
             <div className="">
-                <h1 className="text-xl font-semibold text-indigo-800 my-4">Today</h1>
-                <div className="ps-4 border-l-2 border-dashed border-indigo-200 sticky top-6 z-20">
-                    <h1 className="text-base font-medium text-indigo-700 sticky top-0 ">Morning</h1>
-                    <div className="h-full w-full  rounded-xl grid grid-cols-4 gap-4 py-4" >
-                        <NoteCard />
-                        <NoteCard />
-                        <NoteCard />
-                        <NoteCard />
-
+                {timeOfDay && (
+                    <div className="border-l-0 border-dashed border-indigo-200 sticky top-6 z-20">
+                        <h1 className="text-base font-medium text-indigo-700">{timeOfDay} Tasks</h1>
+                        <div className="grid grid-cols-4 gap-4 py-4">
+                            <NoteCard />
+                            <NoteCard />
+                            <NoteCard />
+                            <NoteCard />
+                        </div>
                     </div>
-                </div>
-
-
-                <div className="ps-4 border-l-2 border-dashed border-indigo-200 ">
-
-                    <h1 className="text-base font-medium text-indigo-700">Afternoon</h1>
-                    <div className="h-full w-full  rounded-xl grid grid-cols-4 gap-4 py-4" >
-                        <NoteCard />
-                        <NoteCard />
-                        <NoteCard />
-                        <NoteCard />
-
-                    </div>
-                </div>
-
-                <div className="ps-4 border-l-2 border-dashed border-indigo-200">
-
-                    <h1 className="text-base font-medium text-indigo-700 ">Evening</h1>
-                    <div className="h-full w-full  rounded-xl grid grid-cols-4 gap-4 py-4" >
-                        <NoteCard />
-                        <NoteCard />
-                        <NoteCard />
-                        <NoteCard />
-
-                    </div>
-                </div>
+                )}
 
             </div>
             <div className="">
@@ -79,6 +75,6 @@ export default function Hello() {
 
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
